79bbcdd805fb193ac35d528833605e93e3107b6f
jerin.t <jerin.t@iprotecs.net>
2024-02-06 19:00:30 +0530

    sessionIDS

--- index.js
+++ index.js
@@ -17,15 +17,20 @@ const PORT = process.env.PORT || 4000;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+const connectedClients = [];
 const io = new Server(httpServer, {
   cors: {
     origin: ["https://next-api-ruby.vercel.app", "http://localhost:3001"],
   },
 });
 io.on("connection", (socket) => {
+  connectedClients.push({ id: socket.id });
   console.log("User connected");
   socket.on("disconnect", () => {
     console.log("User Disconnected");
+    connectedClients = connectedClients.filter(
+      (client) => client.id !== socket.id
+    );
   });
   socket.on("message", async (obj) => {
     await Message.create(obj);
