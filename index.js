3000c501c3a2e0f450af17e4d0cbf1cdd00ddce7
JerinOfficial2001 <jerinjerijree@gmail.com>
2024-02-23 10:53:15 +0530

    get call

--- index.js
+++ index.js
@@ -13,59 +13,16 @@ const db = process.env.MONGO_DB;
 mongoose.connect(db).then(() => {
   console.log("DB Connected");
 });
-const options = [
-  cors({
-    origin: "*",
-    methods: "*",
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }),
-];
 
-app.use(options);
 const PORT = process.env.PORT || 4000;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-var connectedClients = [];
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"], // Specify allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"],
-  },
+const io = new Server(httpServer);
+app.get("/", (req, res) => {
+  res.write(`<h1>Socket Server is running on:${PORT}</h1>`);
 });
-const sessions = new Map();
-// Set up CORS headers for Express routes
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
-
 io.on("connection", (socket) => {
-  socket.on("authenticate", (sessionId) => {
-    console.log(sessionId);
-    if (sessionId) {
-      // Check if the session ID exists in the session map
-      if (sessions.has(sessionId)) {
-        // Associate the socket with the session ID
-        sessions.set(sessionId, socket.id);
-        console.log(`User authenticated with session ID: ${sessionId}`);
-        socket.emit("authenticated");
-      } else {
-        console.log(`Session ID ${sessionId} not found`);
-        socket.emit("authentication_failed", "Session ID not found");
-      }
-    } else {
-      console.log("Invalid session ID format");
-      socket.emit("authentication_failed", "Invalid session ID");
-    }
-  });
   console.log("User connected");
   socket.on("disconnect", () => {
     console.log("User Disconnected");
