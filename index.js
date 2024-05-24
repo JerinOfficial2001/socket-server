751c5fa627e6f778efd4449df42105611a1ffb0b
JerinOfficial2001 <jerinjerijree@gmail.com>
2024-05-24 18:37:37 +0530

    socket-vchat

--- index.js
+++ index.js
@@ -35,18 +35,22 @@ let activeUsers = [];
 let watchingUsers = [];
 let typingUsers = [];
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
-    console.log(roomId, "roomId");
-    socket.join(roomId.roomID);
-    socket.to(roomId.roomID).emit("user-connected", roomId.id);
-    socket.on("disconnect", () => {
-      console.log("roomIdDisconnect");
-      socket.to(roomId.roomID).emit("user-disconnected", roomId.id);
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      name: data.name,
     });
-    socket.on("signal", (data) => {
-      console.log(data, "signal");
-      io.to(data.target).emit("signal", data);
-    });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 
   io.emit("getNotification", { status: "ok" });
